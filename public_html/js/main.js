f2e683f636b18902a1430e68cac7e4764e7caadf
Andrei Mustata <andrei@streamba.net>
2013-10-10 21:03:55 +0300

    adding session titles in the table

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -29,9 +29,22 @@
         return booking.timeSlot.start;
     });
 
+    var agendaTBody = agendaTable.children("tbody");
     _.each(groupedBookings, function(group, key) {
         group = _.sortBy(group, function(session) {
             return session.track.name;
+        });
+
+        // gaaaah
+        var hours = group[0].timeSlot.start.getHours();
+        hours = hours < 10 ? "0" + hours.toString() : hours.toString();
+        var minutes = group[0].timeSlot.start.getMinutes();
+        minutes = minutes < 10 ? "0" + minutes.toString() : minutes.toString();
+
+        var slotRow = agendaTBody.append("<tr/>");
+        slotRow.append($("<td/>", {text: hours + ":" + minutes}));
+        _.each(group, function(booking) {
+            slotRow.append($("<td/>", {text: booking.session.title}));
         });
     });
 })();
