c54395c436ec6f3e9d34c18d6d8b3183bd63edbb
Andrei Mustata <andrei.mustata@gmail.com>
2013-10-10 15:21:44 +0300

    adding models for Agenda and related

--- public_html/js/models.js
+++ public_html/js/models.js
@@ -5,3 +5,75 @@ function Session(session) {
         speakers: session.speakers || []
     };
 }
+
+function TimeSlot(timeSlot) {
+    return {
+        timeSlotCode: timeSlot.timeSlotCode || Date.now(),
+        start: timeSlot.start || new Date(),
+        end: timeSlot.end || new Date(),
+        displayTracks: timeSlot.displayTracks || false
+    };
+}
+
+function Track(track) {
+    return {
+        name: track.name || "No name"
+    };
+}
+
+function Booking(booking) {
+    return {
+        timeSlot: booking.timeSlot || new TimeSlot(),
+        track: booking.track || new Track(),
+        session: booking.session || new Session(),
+        alt: booking.alt || "alt"
+    };
+}
+
+function Agenda() {
+    var tracks = [],
+        timeSlots = [],
+        bookings = [];
+
+    function getTrack(name) {
+        var filteredTracks = $.map(tracks, function(track) {
+            if (track.name === name) {
+                return track;
+            }
+        });
+
+        return filteredTracks[0] || null;
+    }
+
+    function getTimeSlot(timeSlotCode) {
+        var filteredTimeSlots = $.map(timeSlots, function(timeSlot) {
+            if (timeSlot.timeSlotCode === timeSlotCode) {
+                return timeSlot;
+            }
+        });
+
+        return filteredTimeSlots[0] || null;
+    }
+
+    function getBookingsByTimeSlot(timeSlot, track) {
+        var filteredBookings = $.map(bookings, function(booking) {
+            if (booking.timeSlot.timeSlotCode === timeSlot.timeSlotCode && (track === undefined || booking.track === track)) {
+                return booking;
+            }
+        });
+
+        return filteredBookings;
+    }
+
+    function isOneBookingForAllTracks() {}
+
+    return {
+        tracks: tracks,
+        timeSlots: timeSlots,
+        bookings: bookings,
+        getTrack: getTrack,
+        getTimeSlot: getTimeSlot,
+        getBookingsByTimeSlot: getBookingsByTimeSlot,
+        isOneBookingForAllTracks: isOneBookingForAllTracks
+    };
+}
