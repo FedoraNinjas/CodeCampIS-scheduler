8e315713ff7e6bd33dfae7adef76105f629e4ae2
Andrei Mustata <andrei@streamba.net>
2013-10-10 15:54:22 +0300

    populating agenda

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -3,4 +3,15 @@
     registerSessions(sessions, function(speaker) {
         return speaker;
     });
+
+    var agenda = Agenda();
+    registerAgenda(agenda, function(sessionTitle) {
+        var filteredSessions = $.map(sessions, function(session) {
+            if (session.title === sessionTitle) {
+                return session;
+            }
+        });
+
+        return filteredSessions[0] || null;
+    });
 })();
